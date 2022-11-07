29dd82bccc53c4ccee9649f3bfc7ef9afcd92dd4
bilalcinal <73084109+bilalcinal@users.noreply.github.com>
2022-11-07 18:54:10 +0300

    Project end

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -37,8 +37,75 @@ if(ModelState.IsValid)
 {
 _db.Categories.Add(obj);
 _db.SaveChanges();
+TempData["Success"] ="Category created successfuly";
 return RedirectToAction("Index");
 }
 return View(obj);
+
+}
+   //GET
+public IActionResult Edit(int? id)
+{
+     if(id==null || id==0)
+     {
+        return NotFound();
+     }
+     var CategoryFromDb = _db.Categories.Find(id);
+     if(CategoryFromDb==null)
+     {
+        return NotFound();
+     }
+    return View(CategoryFromDb);
+}
+//POST
+[HttpPost]
+[ValidateAntiForgeryToken]
+public IActionResult Edit(Category obj)
+{
+if(obj.Name==obj.DisplayOrder.ToString())
+{
+    ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+}
+if(ModelState.IsValid)
+{
+_db.Categories.Update(obj);
+_db.SaveChanges();
+TempData["Success"] ="Category updated successfuly";
+
+return RedirectToAction("Index");
+}
+return View(obj);
+}
+   //GET
+public IActionResult Delete(int? id)
+{
+     if(id==null || id==0)
+     {
+        return NotFound();
+     }
+     var CategoryFromDb = _db.Categories.Find(id);
+     if(CategoryFromDb==null)
+     {
+        return NotFound();
+     }
+    return View(CategoryFromDb);
+}
+//POST
+[HttpPost,ActionName("Delete")]
+[ValidateAntiForgeryToken]
+public IActionResult DeletePOST(int? id)
+{
+ var obj=_db.Categories.Find(id);
+ if(obj==null)
+ {
+    return NotFound();
+ }
+_db.Categories.Remove(obj);
+_db.SaveChanges();
+TempData["Success"] ="Category Deleted successfuly";
+
+return RedirectToAction("Index");
+
+
 }
 }
