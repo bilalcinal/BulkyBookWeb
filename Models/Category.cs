ae17e0a0da578ae4d3e440b09f29dd65d949dc61
bilalcinal <73084109+bilalcinal@users.noreply.github.com>
2022-11-07 17:30:20 +0300

    Category Validations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models;
@@ -8,6 +9,8 @@ public class Category
     public int id { get; set; }
     [Required]  
     public string  Name { get; set; }
+    [DisplayName("Display Order")]
+    [Range(1,100,ErrorMessage ="Display Order must be betwenn 1 and 100 only!!")]
     public int DisplayOrder { get; set; }
     public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 
