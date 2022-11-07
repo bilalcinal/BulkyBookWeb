74da6b9d6adf784d63551e650e87193c8cc6a209
bilalcinal <73084109+bilalcinal@users.noreply.github.com>
2022-11-07 16:14:20 +0300

    Bootstrap and category list added

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Category> Categories{ get ; set ; } 
+}
+ 
---
+++ Models/Category.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models;
+
+public class Category
+{
+    [Key]
+    public int id { get; set; }
+    [Required]  
+    public string  Name { get; set; }
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+}
